67dc06804397d5dab909713f1c5bd31204dc7cbf
Elena <elenaevans10@gmail.com>
2020-12-18 15:14:14 +0000

    added new class and spec file for the Station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,13 @@
+require 'station'
+
+describe Station do
+  station = Station.new('Old Street', 1)
+  
+  it 'knows its name' do
+    expect(station.name).to eq 'Old Street'
+  end
+
+  it 'knows its zone' do
+    expect(station.zone).to eq 1
+  end
+end
